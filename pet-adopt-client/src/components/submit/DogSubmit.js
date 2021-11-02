563fc06b24f9b2e6c6a5313f6e75da65db106894
JacobHocker <jacobdouglas06@gmail.com>
2021-11-02 12:55:12 -0500

    working with the forms

--- pet-adopt-client/src/components/submit/DogSubmit.js
+++ pet-adopt-client/src/components/submit/DogSubmit.js
@@ -8,7 +8,7 @@ function DogSubmit({ onAddDog }) {
     const [age, setAge] = useState(0)
     const [price, setPrice] = useState(0)
     const [note, setNote] = useState("")
-    const [shelterID, setShelterID] = useState(0)
+    const [shelterId, setShelterId] = useState(0)
 
     function handleSubmit(event) {
         event.preventDefault()
@@ -24,7 +24,7 @@ function DogSubmit({ onAddDog }) {
                 age: age,
                 price: price,
                 note: note,
-                shelterID: shelterID,
+                shelterId: shelterId,
             }),
         })
         .then((r) => r.json())
@@ -33,6 +33,72 @@ function DogSubmit({ onAddDog }) {
     return(
         <div className='submit-a-dog'> 
             <h1>If you would like to put a dog up for adoption<br />Please fill out the form below!</h1>
+            <form onSubmit={handleSubmit}>
+                <div className='input-dog'>
+                    <input 
+                        className='dog-input'
+                        type="text"
+                        placeholder="Name of the dog"
+                        name="Dog Name"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                    />
+                    <input 
+                        className='dog-input'
+                        type="text"
+                        placeholder="Breed of the dog"
+                        name="Dog Breed"
+                        value={breed}
+                        onChange={(event) => setBreed(event.target.value)}
+                    />
+                    <input 
+                        className='dog-input'
+                        type="text"
+                        placeholder="Image of the dog"
+                        name="Dog Image"
+                        value={imageUrl}
+                        onChange={(event) => setImageUrl(event.target.value)}
+                    />
+                    <input 
+                        className='dog-input'
+                        type="text"
+                        placeholder="Age of the dog"
+                        name="Dog Age"
+                        value={age}
+                        onChange={(event) => setAge(event.target.value)}
+                    />
+                    <input 
+                        className='dog-input'
+                        type="text"
+                        placeholder="$ amount for adoption"
+                        name="Dog Price"
+                        value={price}
+                        onChange={(event) => setPrice(event.target.value)}
+                    />
+                    <textarea
+                        className='submit-dog-area'
+                        name="Dog not"
+                        type="text"
+                        placeholder='Write a quick note about the dog here...'
+                        rows={10}
+                        value={note}
+                        onChange={(event) => setNote(event.target.value)}
+                    />
+                    <input 
+                        className='dog-input'
+                        type="text"
+                        placeholder="Shelter I.D."
+                        name="Dog Shelter I.D."
+                        value={shelterId}
+                        onChange={(event) => setShelterId(event.target.value)}
+                    />
+                </div>
+                <button className='submit-button' type='submit'>
+                    Submit For Adoption!
+                </button>
+            </form>
+        
+        
         </div>
     )
 }
