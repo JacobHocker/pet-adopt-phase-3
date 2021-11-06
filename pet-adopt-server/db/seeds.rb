3239d928fb43737c3144748365af66d0d0af4f45
JacobHocker <jacobdouglas06@gmail.com>
2021-11-06 17:10:14 -0500

    added seed & styling

--- pet-adopt-server/db/seeds.rb
+++ pet-adopt-server/db/seeds.rb
@@ -15,7 +15,7 @@ Shelter.create(
     shelter_img_url: "./images/cap-shelter.jpg",
     address: "12547 West Road, Houston, Tx 77082 USA",
     shelter_notes: "A shelter based around Citizens for Animal Protection. Come by and meet your new best friend. All animals are rescues!",
-    shelter_rating: 4.3,
+    shelter_rating: rand(1..5),
     kill_shelter: "No" 
 )
 Shelter.create(
@@ -24,8 +24,8 @@ Shelter.create(
     name: "Friends For Life Animal Shelter",
     shelter_img_url: "./images/friends-for-life-animal-shelter.jpg",
     address: "14285 Cypress North Houston, Houston, Tx 77041 USA",
-    shelter_notes: "Kill Animals",
-    shelter_rating: 3.9,
+    shelter_notes: "A local shelter dedicated to finding animals & pets the proper homes.",
+    shelter_rating: rand(1..5),
     kill_shelter: "Yes"
 )
 puts 
@@ -54,6 +54,14 @@ Dog.create(
     price: 210,
     note: "Very loyal and obedient dog.",
     shelter_id: 1)
+    Dog.create(
+        name: "Jimmy", 
+        breed: "Siberian Husky", 
+        image_url: "./images/husky-1.jpg",
+        age: 8,
+        price: 500,
+        note: "Smart intuitive dog. Loves the cold & cheeseburgers!",
+        shelter_id: 1)
 puts 
 puts "Creating cats..."
 Cat.create(
